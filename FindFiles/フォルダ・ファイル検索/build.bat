35e357a6deb6fb5b9662105523fea74c0824cc83
yamato1413 <rakisuta08@live.jp>
2022-08-27 21:26:31 +0900

    cscにパスを通して読みやすくした

--- FindFiles/フォルダ・ファイル検索/build.bat
+++ FindFiles/フォルダ・ファイル検索/build.bat
@@ -1,6 +1,6 @@
 @echo off
-
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -target:winexe -reference:Microsoft.VisualBasic.dll %~dp0\FindFiles.cs -win32icon:%~dp0\shell32_55_4.ico
+set PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319;%PATH%
+csc -target:winexe -reference:Microsoft.VisualBasic.dll %~dp0\FindFiles.cs -win32icon:%~dp0\shell32_55_4.ico
 if %ERRORLEVEL% equ 0 (
     .\FindFiles.exe
 )
